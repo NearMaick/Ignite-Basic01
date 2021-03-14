61978b84eff41240542847760911566e31f6e085
Maick Souza <maick_a_s@msn.com>
2021-03-14 12:31:58 -0300

    feat(web): setting webpack and babel #1

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>Maick Souza</h1>
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,19 @@ module.exports = {
   output: {
     path:path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ],
   }
-}
+};
 
 // parei aqui: 2:29
